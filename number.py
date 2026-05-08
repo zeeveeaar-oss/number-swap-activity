c2ef2bc059f195e45f890b1fce7d334b18e93f17
zeeveeaar-oss <zeeveeaar@gmail.com>
2026-05-08 10:54:57 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,12 @@
+# Take input values from user
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+
+# Swapping
+temp = x
+x = y
+y = temp
+
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
